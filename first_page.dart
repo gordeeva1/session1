6408d076eceb35cbb249eb529a3bbf1846bc1095
gordeeva1 <146097537+gordeeva1@users.noreply.github.com>
2023-10-04 21:51:20 +0300

    Update first_page.dart

--- first_page.dart
+++ first_page.dart
@@ -1,23 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:route/first_page.dart';
+import 'package:route/second_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Lesson Route'),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text("Пропустить",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.normal))),
+                  Image.asset("assets/images/shape.png"),
+                ],
+              ),
+              SizedBox(width: 8),
+              const Text(
+                "Анализы",
+                style: TextStyle(color: Colors.green, fontSize: 20),
+              ),
+              const Text(
+                "Экспресс сбор и получение проб",
+                style: TextStyle(color: Colors.black38, fontSize: 14),
+              ),
+              SizedBox(width: 8),
+              ElevatedButton(
+                child: const Text('Далее'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondPage()),
+                  );
+                },
+              ),
+              SizedBox(width: 8),
+              Container(
+                height: MediaQuery.of(context).size.height  / 3,
+                child: Image.asset("assets/images/Illustration.png",fit: BoxFit.contain),
+              ),
+            ],
+          ),
         ),
-        body: first_page(),
       ),
     );
+  }
+}
+
+class FirstBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
