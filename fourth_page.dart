bc7357ddf7c0906227a1207c1d6ce89fc844317d
gordeeva1 <146097537+gordeeva1@users.noreply.github.com>
2023-10-04 14:58:54 +0300

    Create fourth_page.dart

---
+++ fourth_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'five_page.dart';
+
+class fourth_page extends StatelessWidget {
+  const fourth_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              const Text("Добро пожаловать!", style: TextStyle(color: Colors.black, fontSize: 28)),
+              const Text("Войдите, чтобы пользоваться функциями приложения", style: TextStyle(color: Colors.black54, fontSize: 14)),
+          TextField(
+
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'example@mail.ru',
+            ),
+          ),
+              ElevatedButton(
+                child: const Text(' Далее'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const five_page()),
+                  );
+                },
+              ),
+              SizedBox(width: 8),
+              Container(
+                child: Text("Или войдите с помощью", style: TextStyle(color: Colors.black38, fontSize: 14),),
+              ),
+                ElevatedButton(
+                child: const Text('Войти с Яндекс'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const five_page()),
+                  );
+                },
+              ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
